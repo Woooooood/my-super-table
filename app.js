31c1111d3cebf00c9d3c6e63cfee767c21b5d841
Wood <wassim.boulatika@supinternet.fr>
2018-02-25 13:08:33 +0100

    project finished with the 2 bonus options

---
+++ app.js
@@ -0,0 +1,126 @@
+window.onload = function() {
+    var superTable = document.querySelector('#super-table');
+
+    initSuperTable({
+        element: superTable,
+        data: users,
+        options: {
+            firstColumn: 'gender',
+            rowHeight : 30,
+            tableHeight: 70,
+            tableWidth: 70,
+            rowMouseOver: false,
+            rowStyle: 'line',
+            fixedHeader: true,
+            fixedColumn: true
+        }
+    });
+    function initSuperTable(table){
+        var firstLine = document.createElement('div');
+        firstLine.classList.add("firstLine");
+        table.element.appendChild(firstLine);
+
+        for (var i in table.data) {
+            var user = table.data[i];
+        }
+        for (var i in user) {
+            var item = document.createElement('div');
+            item.classList.add(i);
+            firstLine.appendChild(item);
+            item.innerHTML += i;
+        }
+        var users = document.createElement('div');
+        table.element.appendChild(users);
+        users.classList.add('users');
+
+        for (var i in table.data) {
+            user = table.data[i];
+
+            var allUsers = document.createElement('div');
+            allUsers.classList.add("allUsers");
+            users.appendChild(allUsers);
+
+            for (var i in user){
+                var itemUser = document.createElement('div');
+                itemUser.classList.add(i);
+                allUsers.appendChild(itemUser);
+                itemUser.innerHTML = user[i];
+            }
+        }
+
+        var  superTable = document.querySelector('#super-table');
+        superTable.style.height= table.options.tableHeight + "vh";
+        if (table.options.tableHeight > 80) {
+            alert('The maximum  size of the table is 80 !');
+            superTable.style.height= table.options.tableHeight = 80;
+        }
+        superTable.style.width = table.options.tableWidth + "vw";
+
+
+
+        var firstColumn = document.querySelectorAll('.' + table.options.firstColumn);
+        for (var i=0; i < firstColumn.length; i++){
+            firstColumn[i].style.order = "-1";
+        }
+
+        if(table.options.fixedColumn === true){
+            var firstColumn = document.querySelectorAll('.' + table.options.firstColumn);
+            for (var i=0; i < firstColumn.length; i++){
+                firstColumn[i].style.left = '0';
+                firstColumn[i].style.position = 'sticky';
+                firstColumn[i].style.zIndex = '1';
+                firstColumn[i].style.backgroundColor = 'inherit';
+            }
+        }
+
+        if(table.options.fixedHeader === true) {
+            var firstLine = document.querySelector('.firstLine');
+            firstLine.style.position = 'sticky';
+            firstLine.style.top = '0';
+            firstLine.style.zIndex = '1';
+        }
+
+        if (table.options.rowHeight === 'undefined') {
+            var lines = document.querySelectorAll('.allUsers');
+            for (var i = 0; i < lines.length; i++) {
+                lines[i].style.height = '30px';
+            }
+        }
+        var lines = document.querySelectorAll('.allUsers');
+        for (var i=0; i < lines.length; i++){
+            lines[i].style.height = table.options.rowHeight + "px";
+            if (table.options.rowMouseOver === true){
+                lines[i].onmouseover = function () {
+                    this.style.background = '#99151b';
+                };
+                if (table.options === true){
+                    lines[i].onmouseout = function () {
+                        this.style.background = '#000'
+                    }
+                }
+                else lines[i].onmouseout = function () {
+                    this.style.background = '#fff';
+                }
+            }
+
+            if (table.options.rowStyle === "zebra"){
+                lines[i].style.border = '1px solid';
+                var oddNumber = document.querySelectorAll('.allUsers:nth-child(2n+1)');
+                for (var j=0; j < oddNumber.length; j++){
+                    oddNumber[j].style.background = '#118498';
+                    if (table.options.rowMouseOver === true){
+                        oddNumber[j].onmouseout = function () {
+                            this.style.background = '#118498'
+                        }
+                    }
+                }
+            }
+
+            if (table.options.rowStyle === "line"){
+                lines[i].style.border = '1px solid';
+            }
+
+
+        }
+    }
+};
